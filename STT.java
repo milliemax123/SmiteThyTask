5a5856423259e59065ac16fa408ac434d9c77916
Millie <151024437+milliemax123@users.noreply.github.com>
2025-11-16 22:31:46 +0000

    Add files via upload

---
+++ STT.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ * classname: STT.java
+ * rev : 1
+ * date : 7/11/25
+ * @amelia maxwell
+ */
+
+package stt;
+import stt.landingPage;
+
+public class STT {
+public static void main(String[] args) {
+    landingPage myPage = new landingPage();
+    myPage.setVisible(true);
+}
+
+    }
+    
+
